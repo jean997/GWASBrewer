30a91557d77a3ebc3b8443e4de2546671dd39863
Jean Morrison <jeanm@uchicago.edu>
2022-11-21 22:02:24 -0500

    separate generate_F from sim_sumstats_lf, add checks

--- R/generate_F.R
+++ R/generate_F.R
@@ -20,6 +20,43 @@ generate_F_simple <- function(nblocks, type=c("nested", "difference",
   return(F_mat)
 }
 
+#'@title Generate random F
+#'@param g_F Function from which non-zero elements of F are generated
+#'@param nz_factor Number of non-zero elements of each factor if F is to be generated.
+#'@param omega Proportion of trait heritability explained by factors
+#'@param h2_trait Trait heritability
+#'@param pad Add single trait factors? (See details)
+#'@details
+#'Generate a random set of (at least) K factors for M traits.
+#'The number of traits affected by each factor is given by \code{nz_factor}.
+#'Each effect is chosen as a random draw from function g_F.
+#'If any rows of the resulting matrix corresponding to non-zero elements of omega
+#'are all zero and pad  = TRUE, single-trait factors are added.
+#'Finally, the
+#'matrix is re-scaled so that \code{colSums(F_mat^2) = omega*h2_trait}.
+#'@result A matrix
+#'@export
+generate_random_F <- function(K, M, g_F= function(n){runif(n, -1, 1)},
+                              nz_factor, omega, h2_trait,
+                              pad = FALSE){
+  nz_factor <- check_scalar_or_numeric(nz_factor, "nz_factor", K)
+  omega <- check_scalar_or_numeric(omega, "omega", M)
+  h2_trait <- check_scalar_or_numeric(h2_trait, "h2_trait", M)
+  omega <- check_01(omega, "omega")
+  h2_trait <- check_01(h2_trait, "h2_trait")
+  srs <- omega*h2_trait
+  F_mat <- sumstatFactors:::generate_F2(non_zero_by_factor = nz_factor,
+                                        square_row_sums = srs,
+                                        rfunc = g_F)
+  if(any(rowSums(F_mat^2) == 0 & srs != 0) & pad){
+    ix <- which(rowSums(F_mat^2)==0 & srs != 0)
+    stF <- matrix(0, nrow = M, ncol = length(ix))
+    for(i in seq_along(ix)) stF[ix[i],i] <- sign(srs[ix[i]])*sqrt(abs(srs[ix[i]]))
+    F_mat <- cbind(F_mat, stF)
+  }
+  return(F_mat)
+}
+
 #'@export
 generate_F2 <- function(non_zero_by_factor,
                         square_row_sums,
@@ -31,13 +68,16 @@ generate_F2 <- function(non_zero_by_factor,
     x[ix] <- rfunc(nz)
     return(x)
   }
-  stopifnot(all(square_row_sums > 0))
+  ix <- which(square_row_sums > 0)
+  #stopifnot(all(square_row_sums > 0))
 
   M <- length(square_row_sums)
+  Mx <- length(ix)
   K <- length(non_zero_by_factor)
 
-
-  F_mat <- sapply(seq(K), function(k){f(M, non_zero_by_factor[k])})
+  F_mat <- matrix(0, M, K)
+  F_matx <- sapply(seq(K), function(k){f(Mx, non_zero_by_factor[k])})
+  F_mat[ix,] <- F_matx
 
   if(any(rowSums(F_mat !=0)==0)){
     missing_ix <- which(rowSums(F_mat !=0)==0)
@@ -49,5 +89,4 @@ generate_F2 <- function(non_zero_by_factor,
   r2 <- rowSums(F_mat^2)
   F_mat <- F_mat*sqrt(square_row_sums)/sqrt(r2)
   return(F_mat)
-
 }
--- R/sim_sumstats_lf.R
+++ R/sim_sumstats_lf.R
@@ -1,112 +1,120 @@
 
 #See simulating_data.Rmd for explanation
-#'@title Simulate summary stats
-#'@description Simulate summary statistcs for fully overlapping GWAS with no LD
-#'@param F_mat factor matrix M by K
-#'@param N GWAS sample size
+#'@title Simulate summary statistics
+#'@description Simulate summary statistcs from a specified factor structure
+#'@param F_mat factor matrix M by K (M = number of traits, K = number of factors)
+#'@param N GWAS sample size. N can be a scalar, vector, or matrix. If N is a scalar, all GWAS have the same sample size
+#'and there is no overlap between studies. If N is a vector, each element of N specifies the sample size of the corresponding
+#'GWAS and there is no overlap between studies. If N is a matrix, N_ii specifies the sample size of study i
+#' and N_ij specifies the number of samples present in both study i and study j. The elements of N must be positive
+#' but non-integer values will not generate an error.
 #'@param J Total number of SNPs to generate
-#'@param h_2_trait Heritability of each trait. Length M vector.
-#'@param omega Proportion of SNP heritability mediated by factors. Length M vector.
-#'@param h_2_factor Heritability of each factor. Length K vector.
+#'@param h2_trait Heritability of each trait. Length M vector.
+#'@param omega Proportion of trait heritability mediated by factors. Length M vector.
+#'@param h2_factor Heritability of each factor. Length K vector.
 #'@param pi_L Proportion of non-zero elements in L_k. Length K factor
 #'@param pi_theta Proportion of non-zero elements in theta. Scalar or length M vector.
-#'@param R_E Environmental trait correlation not mediated by factors. M by M pd matrix.
-#'@param maf can either be a scalar in which case the same maf is used for all SNPS, NA in which case SNPs
-#'are assumed scaled to variance 1, a function that takes a number n and returns n values between 0 and 1, or a
-#'vector of length equal to the number of SNPs. maf is ignored if LD is provided.
-#'@param R_LD List of eigen decompositions of LD correlation matrices, may be missing.
+#'@param R_E Correlation between environmental trait components not mediated by factors. M by M pd matrix.
+#'@param af Allele frequency (optional). A scalar, a vector, or a function that takes a number n and returns n values between 0 and 1.
+#'This argument is ignored if R_LD and snp_info are provided. If af, R_LD, and snp_info
+#'are all missing, SNPs are assumed to be scaled to variance 1,
+#'@param R_LD List of eigen-decompositions of LD correlation matrices, may be missing.
 #'@param snp_info If R_LD is provided, provide a data frame with columns "SNP" and "AF"
-#'@param g_F Function from which non-zero elements of F are generated
-#'@param nz_factor Number of non-zero elements of each factor if F is to be generated.
 #'@param sporadic_pleiotropy Allow a single SNP to affect multiple factors (default TRUE).
 #'@details
+#'This function will generate GWAS summary statistics for M traits with K common factors.
+#'The matrix F_mat provides the effects of each factor on each trait, \code{F_mat[i,j]}
+#'gives the effect of factor j on trait i. The rows of \code{F_mat} will be scaled in order
+#'to provide desired proportion of hertiability of each trait explained by factors but the relative
+#'size and sign of elements within rows will be retained.
 #'
-#'If F_mat is not provided, it will be generated using the `generate_F2` function.
-#'In this case g_F and nz_factor must be provided. All of the elements
-#'in F are generated iid from a mixture of a point mass at 0 and g_F. The matrix is then
-#'rescaled according to the constraints.
+#'Previously, if F_mat was not provided, a random factor matrix would be generated.
+#'In the current version, this step must be done manually, see examples.
 #'
+#'With these parameters, it is possible to supply a non-feasible set of parameters.
+#'Usually this occurs if the heritability of the factors is low but the heritability
+#'of the traits is high leading to a contradiction. The function will return an error if this happens.
 #'
-#'With this setup it is possible to specify a setting that is impossible. Usually this occurs if the heritability
-#'of the factors is low but the heritability of the traits is high leading to a contradiction. Right now the function
-#'will just return an error if that happens.
+#'Trait covariance: Each trait is composed of four independent components, the genetic component mediated by factors,
+#'the environmental component mediated by factors, the genetic component not mediated by factors,
+#'and the environmental component not mediated by factors. Therefore, the total trait covariance can be decomposed into
+#'the sum of four corresponding covariance matrices. The matrix R_E specifies the correlation of the last component only.
+#'In the returned object, R gives the overall trait correlation matrix multiplied by the overlap proportion matrix,
+#'which is equal to the correlation in the error terms of \code{beta_hat} (see examples).
 #'
+#'@examples
+#'myF <- generate_random_F(K = 3, M = 10, nz_factor = c(2, 3, 2),
+#'                         omega = rep(0.8, 10), h2_trait = rep(0.6, 10), pad = TRUE)
+#'dat <- sim_sumstats_lf(myF, N = 10000, J = 20000, h2_trait = rep(0.6, 10),
+#'                       omega = rep(0.8, 10), pi_L = 0.1, pi_theta = 0.1)
 #'
+#'myF <- diag(2)
+#'N <- matrix(c(10000, 8000, 8000, 10000), nrow = 2)
+#'R_E <- matrix(c(1, 0.6, 0.6, 1), nrow = 2)
+#'dat <- sim_sumstats_lf(F_mat = myF, N = N, J = 20000, h2_trait = rep(0.6, 2), omega = rep(1, 2), h2_factor = rep(1, 2),
+#'                        pi_L = 0.1, pi_theta = 0.1, R_E = R_E)
+#'dat$R
+#'cor(dat$beta_hat[,1]-dat$beta_joint[,1], dat$beta_hat[,2]-dat$beta_joint[,2])
+
+
 #'@export
-sim_sumstats_lf <- function(F_mat, N, J, h_2_trait, omega, h_2_factor,
-                            pi_L, pi_theta,
-                            R_E, maf = NULL, R_LD = NULL, snp_info = NULL,
-                            g_F, nz_factor,
-                            overlap_prop =1,
+sim_sumstats_lf <- function(F_mat, N, J, h2_trait, omega, h2_factor,
+                            pi_L, pi_theta, R_E,
+                            af = NULL, R_LD = NULL, snp_info = NULL,
+                            #g_F, nz_factor,
                             sporadic_pleiotropy = TRUE){
 
-  #Check parameters
-  if(!missing(F_mat)){
-    stopifnot("matrix" %in% class(F_mat))
-    M <- nrow(F_mat)
-    K <- ncol(F_mat)
-    cat("Factor structure provided for ", M, " traits and ", K, " factors.\n")
-  }else{
-    if(missing(g_F) | missing(nz_factor) ){
-      stop("If F_mat is missing please supply g_F and nz_factor")
-    }
-    K <- length(nz_factor)
-    M <- length(h_2_trait)
-    cat("Will generate L and F with ", J, " markers, ", M, " traits, and ", K, " factors.\n")
+  # Argument checks
+  M <- nrow(F_mat)
+  K <- ncol(F_mat)
+
+  h2_trait <- check_scalar_or_numeric(h2_trait, "h2_trait", M)
+  h2_trait <- check_01(h2_trait)
+  if(!missing(h2_factor)){
+    h2_factor <- check_scalar_or_numeric(h2_factor, "h2_factor", K)
+    h2_factor <- check_01(h2_factor)
   }
-  stopifnot(length(h_2_trait) == M)
-  stopifnot(all(h_2_trait <= 1 & h_2_trait >= 0))
+  F_mat <- check_matrix(F_mat, "F_mat", M, K)
+  omega <- check_scalar_or_numeric(omega, "omega", M)
+  omega <- check_01(omega)
+  pi_L <- check_scalar_or_numeric(pi_L, "pi_L", K)
+  pi_L <- check_01(pi_L)
+  pi_theta <- check_scalar_or_numeric(pi_theta, "pi_theta", M)
+  pi_theta <- check_01(pi_theta)
 
-
-  stopifnot(length(omega) == M)
-  stopifnot(all(omega >= 0 & omega <= 1))
-  stopifnot(length(pi_L) == K)
-  stopifnot(all(pi_L <= 1 & pi_L > 0))
-
-  if(length(pi_theta) == 1){
-    pi_theta <- rep(pi_theta, M)
-  }else{
-    stopifnot(length(pi_theta)== M)
-  }
-  stopifnot(all(pi_theta >=0 & pi_theta <=1))
   if(any(omega < 1 & pi_theta == 0)){
-    stop("Omega is less than 1 for trait ", i, " so pi_theta must be greater than 0 for that trait.")
+    i <- which(omega < 1 & pi_theta == 0)
+    stop(paste0("Any traits with omega < 1 (not all heritability explained by factors) must have pi_theta > 0. CHeck traits ",
+                paste0(i, collapse = ",")))
   }
 
   #R_E
-  if(overlap_prop > 0){
+  nn <- check_N(N, M)
+  if(!Matrix::isDiagonal(nn$Nc)){
     if(missing(R_E) | is.null(R_E)){
-      message("R_E not provided but overlap_prop > 0. Using R_E = diag(ntrait) for no environmental covariance.")
+      message("R_E not provided but overlapping samples are specified. Using R_E = diag(ntrait) for no environmental covariance.")
       R_E <- diag(M)
     }
-    if(missing(h_2_factor))('h_2_factor must be provided if overlap_prop > 0.')
-    stopifnot(nrow(R_E) == M & ncol(R_E) == M)
-    stopifnot(Matrix::isSymmetric(R_E))
-    R_E_eig <- eigen(R_E)
-    stopifnot(all(R_E_eig$values >= 0))
-
-    stopifnot(length(h_2_factor) == K)
-    stopifnot(all(h_2_factor <= 1 & h_2_factor >= 0))
+    if(missing(h2_factor))('h2_factor must be provided if there are overlapping samples.')
+    R_E <- check_matrix(R_E, "R_E", M, M)
+    R_E <- check_psd(R_E, "R_E")
+    if(!Matrix::isSymmetric(R_E)) stop("R_E must be sy")
   }
 
-  #N
-  if(length(N) == 1) N <- rep(N, M)
-    else stopifnot(length(N) == M)
-
-  #maf
+  #af
   if(missing(R_LD) | is.null(R_LD)){
-    if(is.null(maf)){
+    if(is.null(af)){
       sx <- rep(1, J)
-    }else if(class(maf) == "numeric"){
-      stopifnot(length(maf) %in% c(1, J))
-      sx <- sqrt(2*maf*(1-maf))
-      if(length(sx) == 1) sx <- rep(sx, J)
-    }else if(class(maf) == "function"){
-      af <- maf(J)
+    }else if(class(af) == "function"){
+      myaf <- af(J)
+      sx <- sqrt(2*myaf*(1-myaf))
+    }else{
+      af <- check_scalar_or_numeric(af, "af", J)
+      af <- check_01(af)
       sx <- sqrt(2*af*(1-af))
     }
   }else{
-    if(missing(snp_info) | is.null(snp_info)) stop("Please prvide snp_info to go with R_LD.")
+    if(missing(snp_info) | is.null(snp_info)) stop("Please provide snp_info to go with R_LD.")
     l <- sapply(R_LD, function(e){length(e$values)})
     stopifnot(nrow(snp_info) == sum(l))
     stopifnot(all(c("SNP", "AF") %in% names(snp_info)))
@@ -115,27 +123,18 @@ sim_sumstats_lf <- function(F_mat, N, J, h_2_trait, omega, h_2_factor,
 
 
   #Re-scale F or generate it if it is missing
-  if(missing(F_mat)){
-    F_mat <- sumstatFactors:::generate_F2(non_zero_by_factor = nz_factor,
-                         square_row_sums = omega*h_2_trait,
-                         rfunc = g_F)
-    if(any(rowSums(F_mat^2) == 0)){
-      ix <- which(rowSums(F_mat^2)==0)
-      omega[ix] <- 0
-    }
-  }else{
-    if(any(rowSums(F_mat == 0) == K & omega >0)){
+  if(any(rowSums(F_mat == 0) == K & omega >0)){
       stop("One row of F is zero but corresponds to non-zero omega\n")
-    }
-    #Re scale rows of F
-    scale <- sqrt(omega*h_2_trait/rowSums(F_mat^2))
-    scale[omega == 0] <- 0
-    F_mat <- F_mat*scale
   }
+  #Re scale rows of F
+  scale <- sqrt(omega*h2_trait/rowSums(F_mat^2))
+  scale[omega == 0] <- 0
+  F_mat <- F_mat*scale
+
 
 
   #Generate theta
-  sigma_theta <- sqrt( (1/(pi_theta*J)) * (1-omega)*h_2_trait)
+  sigma_theta <- sqrt( (1/(pi_theta*J)) * (1-omega)*h2_trait)
   sigma_theta[omega == 1] <- 0
 
   if(all(sigma_theta == 0)){
@@ -150,7 +149,7 @@ sim_sumstats_lf <- function(F_mat, N, J, h_2_trait, omega, h_2_factor,
   }else{
     ix <- which(sigma_theta > 0)
     if(sum(pi_theta[ix]) > 1){
-      stop("You have requested too many traits and too many causal variants to use sporadic_pleiotrop = FALSE.\n")
+      stop("You have requested too many traits and too many causal variants to use sporadic_pleiotropy = FALSE.\n")
     }
     p <- sum(pi_theta[ix])
     nz_theta_ix <- sample(c(0, ix), size = J, replace = TRUE, prob = c(1-p, pi_theta[ix]) )
@@ -162,8 +161,6 @@ sim_sumstats_lf <- function(F_mat, N, J, h_2_trait, omega, h_2_factor,
       return(val)
     }) %>% do.call(cbind, .)
   }
-
-
 
   #Generate L
   sigma_L <- sqrt(1/(pi_L*J))
@@ -190,124 +187,63 @@ sim_sumstats_lf <- function(F_mat, N, J, h_2_trait, omega, h_2_factor,
   }
 
 
-  # Compute Beta, standardized effects
+  # Compute standardized effects
   # Since phenos are scaled to variance 1, sqrt(N_m)*beta_{j,m} = z_{j,m}
   beta_std = L_mat %*% t(F_mat) + theta
-  true_h2 = colSums(beta_std^2)
-  Z <- beta_std %*% diag(sqrt(N), nrow = M)
+  true_h2 = colSums(beta_std^2) # this assumes no ld
 
   #Compute row covariance
-  if(overlap_prop > 0){
+  if(!Matrix::isDiagonal(nn$Nc)){
     Sigma_G <- F_mat %*% t(F_mat) + J*diag(pi_theta*sigma_theta^2)
 
-    sigma_2_F <- (1-h_2_factor)/(h_2_factor)
-    Sigma_FE <- F_mat %*% diag(sigma_2_F) %*% t(F_mat)
+    sigma2_F <- (1-h2_factor)/(h2_factor)
+    Sigma_FE <- F_mat %*% diag(sigma2_F) %*% t(F_mat)
 
-    if(any(h_2_trait + diag(Sigma_FE) > 1)){
-      stop("Provided parameters are incompatible with generated F.\n")
+    if(any(h2_trait + diag(Sigma_FE) > 1)){
+      stop("Provided parameters are incompatible with F_mat.\n")
     }
 
-    sigma_E <- sqrt(1 - h_2_trait - diag(Sigma_FE))
+    sigma_E <- sqrt(1 - h2_trait - diag(Sigma_FE))
     Sigma_E <- diag(sigma_E) %*% R_E %*% diag(sigma_E)
     #correlation of z-scores
     R <- Sigma_G + Sigma_FE + Sigma_E
-    R <- overlap_prop*R + (1- overlap_prop)*diag(M)
+    R <- nn$Nc*R
+    #R <- overlap_prop*R + (1- overlap_prop)*diag(M)
 
     # Covariance of normalized effects
     # Sigma <- diag(sqrt(1/N)) %*% R %*% diag(sqrt(1/N))
 
     # Compute proportion of environmental variance from factors
-    tau <- diag(Sigma_FE)/(1-h_2_trait)
+    #tau <- diag(Sigma_FE)/(1-h2_trait)
   }else{
     R <- diag(M)
-    tau <- NULL
+    #tau <- NULL
     R_E = NULL
   }
-  #Generate sampling error
-  E_Z <- MASS::mvrnorm(n=J, mu = rep(0, M), Sigma = R)
 
-  #Generate summary statistics
-  if(missing(R_LD) | is.null(R_LD)){
-    se_beta_hat <- matrix(1/sx) %*% matrix(1/sqrt(N), nrow = 1) # J by M
-    beta_hat <- (Z + E_Z)*se_beta_hat
-    # Convert L and theta from standardized scale
-    L_mat <- ((1/sx)*matrix(1, nrow = J, ncol = K))*L_mat
-    theta <- ((1/sx)*matrix(1, nrow = J, ncol = M))*theta
+  sum_stats <- gen_bhat_from_b(b_joint_std = beta_std, R = R,  N = nn$N,
+                               R_LD = R_LD, snp_info = snp_info, maf = af,
+                               L_mat_joint_std = L_mat,
+                               theta_joint_std = theta)
 
-    ret <- list(beta_hat =beta_hat, se_beta_hat = se_beta_hat,
-                L_mat = L_mat, F_mat = F_mat, theta = theta,
-                L_mat_direct = L_mat, theta_direct = theta,
-                R_E = R_E, tau = tau, R=R, Z = Z, true_h2 = true_h2)
-    return(ret)
+  beta_marg <- with(sum_stats, Z*se_beta_hat)
+  ret <- list(beta_hat =sum_stats$beta_hat,
+              se_beta_hat = sum_stats$se_beta_hat,
+              L_mat = sum_stats$L_mat,
+              F_mat = F_mat,
+              theta = sum_stats$theta,
+              L_mat_joint = L_mat/sum_stats$sx,
+              theta_joint = theta/sum_stats$sx,
+              beta_joint = beta_std/sum_stats$sx,
+              beta_marg = beta_marg,
+              R_E = R_E, #tau = tau,
+              R=R,
+              true_h2 = true_h2,
+              sx = sum_stats$sx)
+
+  if(!is.null(R_LD)){
+    ret$snp_info <- sum_stats$snp_info
   }
-
-  #If LD, introduce correlation
-
-  #Figure out how much/how many replicates of supplied LD we need
-  nblock <- length(R_LD)
-  l <- sapply(R_LD, function(e){length(e$values)})
-  ld_size <- sum(l)
-  full_reps <- floor(J/ld_size) # Recall l is list of block sizes
-  remainder <- J  - full_reps*ld_size
-  blocks_rem <- max(which(cumsum(l) <= remainder)) # full blocks in last partial repeat
-  final_remainder <- remainder-cumsum(l)[blocks_rem] # partial block in last partial repeat
-
-  last_block <- with(R_LD[[blocks_rem + 1]], (vectors %*% diag(values) %*% t(vectors))[1:final_remainder, 1:final_remainder])
-  R_LD[[nblock + 1]] <- eigen(last_block)
-  block_index <- c(rep(seq(nblock), full_reps), seq(blocks_rem), nblock + 1)
-  l <- c(l, final_remainder)[block_index] # l is now lengths of blocks in data
-  start_ix <- cumsum(c(1, l[-length(l)]))
-  end_ix <- start_ix + l-1
-
-  #snp info
-  snp_info_full <- snp_info[c(rep(seq(ld_size), full_reps), seq(remainder)),]
-  if(full_reps == 0){
-    snp_info_full$rep <- rep(1, remainder)
-  }else{
-    snp_info_full$rep <- c(rep(seq(full_reps), each = ld_size), rep(full_reps + 1, remainder))
-  }
-  snp_info_full$SNP <- with(snp_info_full, paste0(SNP, ".", rep))
-  sx <- with(snp_info_full, sqrt(2*AF*(1-AF)))
-
-  # Multiply errors by square root of LD matrix
-  E_LD_Z <- lapply(seq_along(block_index), function(i){
-    with(R_LD[[block_index[i]]], vectors %*% sqrt(diag(values)) %*% E_Z[start_ix[i]:end_ix[i], ])
-  }) %>% do.call( rbind, .)
-
-  # Transform Z by LD matrix
-  Z <- lapply(seq_along(block_index), function(i){
-    with(R_LD[[block_index[i]]], vectors %*% diag(values) %*% t(vectors) %*% Z[start_ix[i]:end_ix[i], ])
-  }) %>% do.call( rbind, .)
-  Z_hat <- Z + E_LD_Z
-
-
-  se_beta_hat <- matrix(1/sx) %*% matrix(1/sqrt(N), nrow = 1) # J by M
-  beta_hat <- Z_hat*se_beta_hat
-
-  # Convert L and Theta to observed scale by dividing by se of genotypes
-  L_mat <- L_mat_direct <-  ((1/sx)*matrix(1, nrow = J, ncol = K))*L_mat
-  theta <- theta_direct <- ((1/sx)*matrix(1, nrow = J, ncol = M))*theta
-
-  # Transform L by LD matrix
-  L_mat <- L_mat*sx # S^-inv L (the N will cancel)
-  L_mat <- lapply(seq_along(block_index), function(i){
-    with(R_LD[[block_index[i]]],
-         vectors %*% diag(values) %*% t(vectors) %*% L_mat[start_ix[i]:end_ix[i], ])
-  }) %>% do.call( rbind, .)
-  L_mat <- L_mat/sx
-
-  # Transform Theta by LD matrix
-  theta <- theta*sx
-  theta <- lapply(seq_along(block_index), function(i){
-    with(R_LD[[block_index[i]]], vectors %*% diag(values) %*% t(vectors) %*% theta[start_ix[i]:end_ix[i], ])
-  }) %>% do.call( rbind, .)
-  theta <- theta/sx
-
-  ret <- list(beta_hat =beta_hat, se_beta_hat = se_beta_hat, Z = Z,
-              L_mat = L_mat, F_mat = F_mat, theta = theta,
-              L_mat_direct = L_mat_direct, theta_direct = theta_direct,
-              R_E = R_E, tau = tau, R = R, snp_info = snp_info_full,
-              true_h2 = true_h2)
   return(ret)
 }
 
