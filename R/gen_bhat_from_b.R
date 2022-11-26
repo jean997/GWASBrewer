a4e3a30effb128ace7cd7faebff791fae8e285c1
Jean Morrison <morrison.jeanv@gmail.com>
2022-11-26 15:50:45 -0500

    add b_joint input option, use check functions

--- R/gen_bhat_from_b.R
+++ R/gen_bhat_from_b.R
@@ -1,48 +1,59 @@
 #'@title Generate beta hats from standardized direct SNP effects and LD
 #'@param b_joint_std variants by traits matrix of direct joint SNP effects.
-#'@param R traits by traits correlation matrix. This gives the correlation of z-scores due to sample overlap and environmental correlation.
-#'This is output as the R object from sim_mv.
 #'@export
-gen_bhat_from_b <- function(b_joint_std, R,  N,
-                            R_LD = NULL, snp_info = NULL, maf = NULL,
+gen_bhat_from_b <- function(b_joint_std, b_joint,
+                            trait_corr,  N,
+                            R_LD = NULL, snp_info = NULL, af = NULL,
                             L_mat_joint_std = NULL, theta_joint_std = NULL){
 
-  if(!"matrix" %in% class(b_joint_std)){
-    stop("b_joint_std must have class matrix\n")
-  }
-  if(!"matrix" %in% class(R)){
-    stop("R must have class matrix\n")
+  if(!missing(b_joint)){
+    if(!missing(beta_joint_std)){
+      stop("Please provide only one of b_joint or b_joint_std")
+    }
+    if(!"matrix" %in% class(b_joint)){
+      stop("b_joint_std must have class matrix\n")
+    }
+    M <- ncol(b_joint)
+    J <- nrow(b_joint)
+    b_type <- "non_std"
+  }else if(!missing(b_joint_std)){
+    if(!"matrix" %in% class(b_joint_std)){
+      stop("b_joint_std must have class matrix\n")
+    }
+    # Expected z-score
+    M <- ncol(b_joint_std)
+    J <- nrow(b_joint_std)
+    b_type <- "std"
   }
 
-  M <- ncol(b_joint_std)
-  J <- nrow(b_joint_std)
   message(paste0("SNP effects provided for ", J, " SNPs and ", M, " traits."))
 
-  if(length(N) == 1) N <- rep(N, M)
-  else if(length(N) != M){
-    stop(paste0("Inconsistent arguments: N has length ", length(N), " but b_joint_std has ", M, " columns."))
-  }
-  if(nrow(R) != M | ncol(R) != M | !Matrix::isSymmetric(R) | !all(diag(R) == 1)){
-    stop(paste0("R should be an ", M , " times ", M, " correlation matrix."))
-  }
+  nn <- check_N(N, M)
+  trait_corr <- check_matrix(trait_corr, "trait_corr", M, M)
+  trait_corr <- check_psd(trait_corr, "trait_corr")
 
 
+  # Correlation due to sample overlap is here
+  R <- nn$Nc*trait_corr
+  # sampling error on z-score without LD
   E_Z <- MASS::mvrnorm(n=J, mu = rep(0, M), Sigma = R)
-  Z <- t(t(b_joint_std)*sqrt(N))
 
-  if(missing(R_LD) | is.null(R_LD)){
-
-    # generate MAF if required and compute sqrt(var(genos))
-    if(is.null(maf)){
+  if(is.null(R_LD)){
+    # compute sqrt(var(genos))
+    if(is.null(af)){
       sx <- rep(1, J)
-    }else if(class(maf) == "numeric"){
-      stopifnot(length(maf) %in% c(1, J))
-      sx <- sqrt(2*maf*(1-maf))
+    }else{
+      af <- check_scalar_or_numeric(af, "af", J)
+      sx <- sqrt(2*af*(1-af))
       if(length(sx) == 1) sx <- rep(sx, J)
     }
-
-
-    se_beta_hat <- kronecker(matrix(1/sx), matrix(1/sqrt(N), nrow = 1))
+    # se(beta_hat)_{ij} = 1/(sqrt(N_j)*sd(x_i))
+    se_beta_hat <- kronecker(matrix(1/sx), matrix(1/sqrt(nn$N), nrow = 1))
+    if(b_type == "non_std"){
+      Z <- b_joint/se_beta_hat
+    }else{
+      Z <- t(t(b_joint_std)*sqrt(nn$N))
+    }
     beta_hat <- (Z + E_Z)*se_beta_hat
 
     ret <- list(beta_hat =beta_hat,
@@ -62,33 +73,33 @@ gen_bhat_from_b <- function(b_joint_std, R,  N,
 
   #If LD,
 
-  if(missing(snp_info) | is.null(snp_info)) stop("Please prvide snp_info to go with R_LD.")
+  if(is.null(snp_info)) stop("Please provide snp_info to go with R_LD.")
+  R_LD <- check_R_LD(R_LD)
   l <- sapply(R_LD, function(e){length(e$values)})
-  nblock <- length(R_LD)
-
-  stopifnot(nrow(snp_info) == sum(l))
-  stopifnot(all(c("SNP", "AF") %in% names(snp_info)))
-  snp_info$block <- rep(seq(length(l)), l)
-  snp_info$ix_in_block <- sapply(l, function(ll){seq(ll)}) %>% unlist()
-
-  #If LD, introduce correlation
+  snp_info <- check_snpinfo(snp_info, l)
+  nblock <- length(l)
 
   #Bookkeeping: Figure out how much/how many replicates of supplied LD we need
   ld_size <- sum(l)
   full_reps <- floor(J/ld_size) # Recall l is list of block sizes
   remainder <- J  - full_reps*ld_size
 
+  block_index <- rep(seq(nblock), full_reps)
   if(remainder > 0){
-    blocks_rem <- max(which(cumsum(l) <= remainder)) # full blocks in last partial repeat
-    final_remainder <- remainder-cumsum(l)[blocks_rem] # partial block in last partial repeat
-
+    if(l[1] >= remainder){
+      blocks_rem <- 0
+      final_remainder <- remainder
+    }else{
+      blocks_rem <- max(which(cumsum(l) <= remainder)) # full blocks in last partial repeat
+      final_remainder <- remainder-cumsum(l)[blocks_rem] # partial block in last partial repeat
+      block_index <- c(block_index, seq(blocks_rem))
+    }
     if(final_remainder > 0){
+      block_index <- c(block_index, nblock + 1)
       last_block <- with(R_LD[[blocks_rem + 1]], (vectors %*% diag(values) %*% t(vectors))[1:final_remainder, 1:final_remainder])
       R_LD[[nblock + 1]] <- eigen(last_block)
-      block_index <- c(rep(seq(nblock), full_reps), seq(blocks_rem), nblock + 1)
       l <- c(l, final_remainder)[block_index] # l is now lengths of blocks in data
     }else{
-      block_index <- c(rep(seq(nblock), full_reps), seq(blocks_rem))
       l <- l[block_index] # l is now lengths of blocks in data
     }
     #Create SNP info for full data set
@@ -104,18 +115,22 @@ gen_bhat_from_b <- function(b_joint_std, R,  N,
   if(full_reps == 0){
     snp_info_full$rep <- 1
   }
-  start_ix <- cumsum(c(1, l[-length(l)]))
-  end_ix <- start_ix + l-1
 
   snp_info_full$SNP <- with(snp_info_full, paste0(SNP, ".", rep))
   sx <- with(snp_info_full, sqrt(2*AF*(1-AF)))
-
+  se_beta_hat <- kronecker(matrix(1/sx), matrix(1/sqrt(nn$N), nrow = 1)) # J by M
 
   # Multiply errors by square root of LD matrix
   E_LD_Z <- lapply(seq_along(block_index), function(i){
     #with(R_LD[[block_index[i]]], vectors %*% diag(sqrt(values)) %*% E_Z[start_ix[i]:end_ix[i], ])
     with(R_LD[[block_index[i]]], tcrossprod(vectors, tcrossprod(t(E_Z[start_ix[i]:end_ix[i], ]), diag(sqrt(values)))))
   }) %>% do.call( rbind, .)
+
+  if(b_type == "non_std"){
+    Z <- b_joint/se_beta_hat
+  }else{
+    Z <- t(t(b_joint_std)*sqrt(nn$N))
+  }
 
   # Transform Z by LD matrix
   Z <- lapply(seq_along(block_index), function(i){
@@ -124,8 +139,6 @@ gen_bhat_from_b <- function(b_joint_std, R,  N,
   }) %>% do.call( rbind, .)
 
   Z_hat <- Z + E_LD_Z
-
-  se_beta_hat <- kronecker(matrix(1/sx), matrix(1/sqrt(N), nrow = 1)) # J by M
   beta_hat <- Z_hat*se_beta_hat
 
   ret <- list(beta_hat =beta_hat,
