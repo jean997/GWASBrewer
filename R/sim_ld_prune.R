4d79ee50c87138674f7134b6168e84ad0980bf93
Jean Morrison <morrison.jeanv@gmail.com>
2022-11-26 20:34:26 -0500

    ld functions are faster now

--- R/sim_ld_prune.R
+++ R/sim_ld_prune.R
@@ -1,9 +1,11 @@
 #'@title LD prune simulated data
 #'@param dat Data object produced by sim_mv
-#'@param pvalue A vector of p-values to use to prioritize variants.
-#'@param R_LD The same list of eigen-decompositions used to generate dat
-#'@param r2_thresh r^2 thershold for pruning
-#'@param pval_thresh p-value threshod for pruning (see details)
+#'@param pvalue Either a vector used to prioritize variants or an integer.
+#'If pvalue is an integer, i, variants will be priorized by the p-value for trait i.
+#'If pvalue is missing, variants will be prioritized randomly.
+#'@param R_LD LD pattern used to generate dat
+#'@param r2_thresh r^2 threshold for pruning
+#'@param pval_thresh p-value threshold for pruning (see details)
 #'@details Given results from sim_mv, and a vector of p-values,
 #'the function will return a list of variants that have \code{p < pval_thresh}
 #'and which mutually have squared correlation less than \code{r2_thresh}.
@@ -25,60 +27,105 @@
 #'@export
 sim_ld_prune <- function(dat, pvalue, R_LD, r2_thresh = 0.1, pval_thresh = 1){
   if(missing(pvalue)){
-    p <- 2*pnorm(-abs(dat$beta_hat/dat$se_beta_hat))
-    pvalue <- apply(p, 1, min)
+    if(!pval_thresh == 1){stop("If providing pval_thresh, please also provide pvalue.\n")}
+    message("pvalue omitted so variants will be prioritized randomly")
+    pvalue <- runif(n = nrow(dat$beta_hat))
   }else if(length(pvalue) ==1){
+    message(paste0("Prioritizing variants based on p-value for trait ", pvalue, "\n"))
     stopifnot(pvalue == round(pvalue))
     p <- 2*pnorm(-abs(dat$beta_hat/dat$se_beta_hat))
     pvalue <- p[,pvalue]
   }
 
-  R_LD <- lapply(R_LD, function(e){e$vectors %*% diag(e$values) %*% t(e$vectors) })
+
+  R_LD <- check_R_LD(R_LD, return = "matrix")
+  ii <- lapply(R_LD, function(R){
+    which(R^2 > r2_thresh, arr.ind = TRUE) %>%
+      data.frame()
+  })
+
   thresh <- sqrt(r2_thresh)
   combos <- expand.grid(block = unique(dat$snp_info$block),
                         rep = unique(dat$snp_info$rep))
+  dat$snp_info$ix_in_dat <- seq(nrow(dat$snp_info))
+
   keep_list <- purrr::map2(combos$block, combos$rep, function(b, r){
-    ix_rem <- which(dat$snp_info$block == b & dat$snp_info$rep == r)
-    myld <- reshape2::melt(R_LD[[b]])
-    myld$ix1 <- ix_rem[myld$Var1]
-    myld$ix2 <- ix_rem[myld$Var2]
+    d <- filter(dat$snp_info, block == b & rep == r) %>% select(ix_in_block, ix_in_dat) %>% arrange(ix_in_block)
+    d$pval <- pvalue[d$ix_in_dat]
+    df_rem <- d %>%
+      arrange(pval) %>%
+      filter(pval < pval_thresh)
+    # myld <- melt_to_df(R_LD[[b]]^2)
+    # myld$ix_in_dat1 <- d$ix_in_dat[myld$row]
+    # myld$ix_in_dat2 <- d$ix_in_dat[myld$col]
+    # myld <- filter(myld, ix_in_dat1 %in% df_rem$ix_in_dat & ix_in_dat2 %in% df_rem$ix_in_dat)
+    # ix_keep <- c()
+    # while(nrow(df_rem) > 0){
+    #   s <- df_rem$ix_in_dat[1]
+    #   ix_keep <- c(ix_keep, s)
+    #   a <- filter(myld, (ix_in_dat1 == s | ix_in_dat2 == s) & abs(value) > thresh)
+    #   v <- unique(a$ix_in_dat1, a$ix_in_dat2)
+    #   df_rem <- filter(df_rem, !ix_in_dat %in% v)
+    # }
+
     ix_keep <- c()
-    df_rem <- data.frame(ix = ix_rem, pval = pvalue[ix_rem]) %>%
-      arrange(pval)
-    df_rem <- filter(df_rem, pval < pval_thresh)
     while(nrow(df_rem) > 0){
-      s <- df_rem$ix[1]
+      s <- df_rem$ix_in_dat[1]
+      sb <- df_rem$ix_in_block[1]
       ix_keep <- c(ix_keep, s)
-      a <- filter(myld, (ix1 == s | ix2 == s) & abs(value) > thresh)
-      v <- unique(a$ix1, a$ix2)
-      df_rem <- filter(df_rem, !ix %in% v)
+      a <- ii[[b]]$col[ii[[b]]$row == sb]
+      df_rem <- filter(df_rem, !ix_in_block %in% a)
     }
+
     return(ix_keep)
   }) %>% unlist()
   return(keep_list)
 }
 
+# A faster version of melt
+melt_to_df <- function(x){
+  data.frame(row=seq(nrow(x))[row(x)],
+             col=seq(ncol(x))[col(x)],
+             value=as.numeric(x))
+}
+
 #'@title Retrieve LD proxies
-#'@param snpinfo snp_info data frame from a simulation object produced by sim_mv
+#'@param dat Simulation object produced by `sim_mv`
 #'@param index list of indexes to retrieve proxies for
-#'@param R_LD list of eigen-decompositions used originally in sim_mv
+#'@param R_LD LD pattern used to generate dat
 #'@param r2_thresh Get proxies with r^2 >= r2_thresh with one of the index variants
 #'@export
-sim_ld_proxy <- function(snpinfo, index, R_LD, r2_thresh = 0.64){
+sim_ld_proxy <- function(dat, index, R_LD, r2_thresh = 0.64, return_mat = FALSE){
+  R_LD <- check_R_LD(R_LD, "matrix")
+  dat$snp_info$ix_in_dat <- seq(nrow(dat$snp_info))
+  ii <- list()
+  blocks <- unique(dat$snp_info[index,]$block)
+  ii[blocks] <- lapply(blocks, function(b){
+    which(R_LD[[b]]^2 > r2_thresh, arr.ind = TRUE) %>%
+      data.frame() %>%
+      filter(row  != col)
+  })
   proxy_info <- lapply(index, function(i){
-    b <- snpinfo$block[i]
-    r <- snpinfo$rep[i]
-    i <- snpinfo$ix_in_block[i]
-    R <- with(R_LD[[b]], vectors %*% diag(values) %*% t(vectors))
-    ii <- which(R[i,]^2 > r2_thresh)
-    return(list(block = b, rep = r, index = i, proxy_index = ii, Rproxy = R[ii,ii]))
+    b <- dat$snp_info$block[i]
+    r <- dat$snp_info$rep[i]
+    id <- dat$snp_info$ix_in_block[i]
+    proxy_index_block <- ii[[b]]$col[ii[[b]]$row == id] %>% sort()
+    proxy_index_dat <- filter(dat$snp_info, rep == r & block == b & ix_in_block %in% proxy_index_block) %>%
+                       arrange(ix_in_block) %>%
+                       with(., ix_in_dat)
+    ret <- list( index = i,
+                # block = b, rep = r,
+                #proxy_index_block = proxy_index_block,
+                proxy_index = proxy_index_dat)
+    if(return_mat) ret$Rproxy <- R_LD[[b]][proxy_index_block, proxy_index_block]
+    return(ret)
   })
   return(proxy_info)
 }
 
 #'@title Extract LD matrix from simulated data
 #'@description Extract LD matrix for specific variants in simulated data set
-#'@param snpinfo snp_info table from data object created by sim_mv
+#'@param dat Simulation object produced by `sim_mv`
 #'@param index vector of indices for snps to extract LD
 #'@param R_LD List of eigen-decompositions used in original simulation
 #'@return An LD matrix. SNP order matches original index order
@@ -98,19 +145,19 @@ sim_ld_proxy <- function(snpinfo, index, R_LD, r2_thresh = 0.64){
 #'dim(ld_mat)
 #'length(index)
 #'@export
-sim_extract_ld <- function(snpinfo, index, R_LD){
+sim_extract_ld <- function(dat, index, R_LD){
   if(!length(index) == length(unique(index))){
     stop("index should contain only unique indices (no duplicates)\n")
   }
-  snpinfo <- snpinfo[index,]
+  R_LD <- check_R_LD(R_LD, "matrix")
+  snpinfo <- dat$snp_info[index,]
   block_sum <- snpinfo %>% group_by(block, rep) %>% summarize(n = n())
   ld_sub_blocks <- map(unique(block_sum$block), function(b){
     r <- block_sum$rep[block_sum$block == b]
-    ldb <- with(R_LD[[b]], tcrossprod(vectors, tcrossprod(vectors, diag(values))))
     sub_sub_blocks <- map(r, function(r){
       i <- filter(snpinfo, block == b& rep == r) %>% with(., ix_in_block)
       s <- filter(snpinfo, block == b& rep == r) %>% with(., SNP)
-      return(list("mat" = ldb[i, i, drop = FALSE], "snp" = s))
+      return(list("mat" = R_LD[[b]][i, i, drop = FALSE], "snp" = s))
     })
     return(sub_sub_blocks)
   }) %>% unlist(recursive = FALSE)
