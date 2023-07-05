9b767aa7ceb837d936268d8741051884ffb46d02
Jean Morrison <jeanm@uchicago.edu>
2023-07-05 15:11:02 -0400

    exposrt compute_h2 and update docs

--- R/compute_h2.R
+++ R/compute_h2.R
@@ -1,3 +1,9 @@
+#'@title Compute hertiability from standardized or non-standardized effects
+#'@param b_joint_std,b_joint matrix of standardized or non-standardized effects. Provide only one of these options.
+#'@param R_LD LD pattern (optional). See \code{?sim_mv} for more details.
+#'@param af Allele frequencies (optional, allowed only if \code{R_LD} is missing). See \code{?sim_mv} for more details.
+#'@param full_mat If TRUE, return the full genetic variance-covariance matrix
+#'@export
 compute_h2 <- function(b_joint_std,
                        b_joint,
                        R_LD = NULL,
