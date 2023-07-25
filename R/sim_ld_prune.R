25dbeb51ccddaacc62f78329308886e72e3c5392
Jean Morrison <morrison.jeanv@gmail.com>
2023-07-24 20:30:53 -0400

    use s_estimate when available

--- R/sim_ld_prune.R
+++ R/sim_ld_prune.R
@@ -27,14 +27,19 @@
 #'prune_set_1 <- sim_ld_prune(dat, pvalue = 1, R_LD = ld_mat_list, pval_thresh = 1e-5)
 #'@export
 sim_ld_prune <- function(dat, pvalue, R_LD, r2_thresh = 0.1, pval_thresh = 1){
+  stopifnot("sim_mv" %in% class(dat))
   if(missing(pvalue)){
     if(!pval_thresh == 1){stop("If providing pval_thresh, please also provide pvalue.\n")}
     message("pvalue omitted so variants will be prioritized randomly")
     pvalue <- runif(n = nrow(dat$beta_hat))
   }else if(length(pvalue) ==1){
-    message(paste0("Prioritizing variants based on p-value for trait ", pvalue, "\n"))
     stopifnot(pvalue == round(pvalue))
-    p <- 2*pnorm(-abs(dat$beta_hat/dat$se_beta_hat))
+    message(paste0("Prioritizing variants based on p-value for trait ", pvalue, "\n"))
+    if(!is.null(dat$s_estimate)){
+      p <- 2*pnorm(-abs(dat$beta_hat/dat$s_estimate))
+    }else{
+      p <- 2*pnorm(-abs(dat$beta_hat/dat$se_beta_hat))
+    }
     pvalue <- p[,pvalue]
   }
 
