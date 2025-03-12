93f349c90f8931e4b8c3c6871b70592cf2b2c50c
Jean Morrison <morrison.jeanv@gmail.com>
2025-03-12 18:01:57 -0400

    updates to package.R

--- R/package.R
+++ R/package.R
@@ -3,6 +3,8 @@
 #' Simulate GWAS summary statistics from specified DAG or factor structure.
 #'
 #' @author Jean Morrison <jvmorr@umich.edu>
-#' @import dplyr purrr tidyr
+#' @import dplyr purrr tidyr 
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib GWASBrewer, .registration = TRUE
 #' @name GWASBrewer
-"_PACKAGE"
+NULL
