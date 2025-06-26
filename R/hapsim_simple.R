e26535d3fbb59321fc56647d27dd9dd8d1b94bd7
Jean Morrison <morrison.jeanv@gmail.com>
2025-06-26 15:46:05 -0400

    new documentation

--- R/hapsim_simple.R
+++ R/hapsim_simple.R
@@ -1,3 +1,15 @@
+
+#' Generate haplotype data from a haplotype object
+#' @description Generates haplotype data from a haplotype object.
+#' @param n The number of haplotypes to generate.
+#' @param hap A haplotype object containing allele frequencies and covariance matrix.
+#' @param seed An optional seed for reproducibility.
+#' @return A matrix of haplotype data, where rows correspond to haplotypes and columns to loci.
+#' @details The function generates haplotype data based on the allele frequencies and covariance matrix provided in the `hap` object. It uses a multivariate normal distribution to generate the haplotypes.
+#' @examples
+#' # Example usage:
+#' hap <- list(freqs = c(0.1, 0.2), cov = matrix(c(1, 0.5, 0.5, 1), nrow = 2))
+#' hapsim_simple(n = 10, hap = hap, seed = 123)
 hapsim_simple <- function(n, hap, seed = NULL){
   if(!is.null(seed))
     set.seed(seed)
@@ -14,7 +26,12 @@ hapsim_simple <- function(n, hap, seed = NULL){
   return(Y)
 }
 
-
+#' Convert LD matrices to haplotype data
+#' @description Converts a list of LD matrices to haplotype data.
+#' @param R_LD A list of LD matrices, each matrix corresponding to a set of loci.
+#' @param af A vector of allele frequencies corresponding to the loci in `R_LD`.  
+#' @return A list of haplotype data, where each element corresponds to a set of loci.
+#' @details The function formats LD matrices into hapsim formatted input data. 
 R_LD_to_haplodat <- function(R_LD, af){
   ld_mat <- check_R_LD(R_LD, "matrix")
   l <- check_R_LD(R_LD, "l")
